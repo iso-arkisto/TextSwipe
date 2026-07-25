8bc6d4660a4cc52e681a9d1f1c5004457defdbc1
Andrey <Fian.123@yandex.ru>
2026-07-25 19:21:50 +0300

    fix: fix gradle build errors preventing launch

--- app/src/main/java/com/yourname/textswipe/MainActivity.kt
+++ app/src/main/java/com/yourname/textswipe/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TextSwipeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "TextSwipe",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
