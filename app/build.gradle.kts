f4ac9e6e8c5c8651d41648579dd92d0a12f1c275
Andrey <Fian.123@yandex.ru>
2026-09-08 15:49:58 +0300

    build: add Material Icons Extended dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,9 @@ dependencies {
     // Coil
     implementation("io.coil-kt:coil-compose:2.6.0")
 
+    // Material Icons Extended
+    implementation("androidx.compose.material:material-icons-extended")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
