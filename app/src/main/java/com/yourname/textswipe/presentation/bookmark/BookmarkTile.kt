517289eff9f96f4f220916e11329a0cd2c0ecbf9
Andrey <Fian.123@yandex.ru>
2026-09-20 11:04:23 +0300

    refactor(presentation): add itemId parameter to BookmarkTile

--- app/src/main/java/com/yourname/textswipe/presentation/bookmark/BookmarkTile.kt
+++ app/src/main/java/com/yourname/textswipe/presentation/bookmark/BookmarkTile.kt
@@ -3,5 +3,6 @@ package com.yourname.textswipe.presentation.bookmark
 data class BookmarkTile(
     val title: String?,
     val category: String,
-    val text: String
+    val text: String,
+    val itemId: String
 )
